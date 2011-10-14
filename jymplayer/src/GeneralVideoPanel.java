99ed6bd60bef9f8d949aaf9d892cfd0ccb310781
ks6g10 <ks6g10@soton.ac.uk>
2011-10-14 02:41:16 +0100

    Added future usage of ComponentListner to add EmptyVideo panels if it fits.

--- jymplayer/src/GeneralVideoPanel.java
+++ jymplayer/src/GeneralVideoPanel.java
@@ -1,3 +1,5 @@
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -88,4 +90,32 @@ public abstract class GeneralVideoPanel extends JPanel {
 		return tmpint;
 	}
 
+}
+
+class detectResize implements ComponentListener {
+
+	@Override
+	public void componentHidden(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentResized(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentShown(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
